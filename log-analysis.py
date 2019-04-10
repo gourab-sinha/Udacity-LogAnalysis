d2a68943b957a8317fe1d2164f714126f399cf3c
Pijush <gourab19964u@gmail.com>
2019-04-10 17:14:22 +0530

    Check pycodestyle errors

--- log-analysis.py
+++ log-analysis.py
@@ -1,6 +1,7 @@
 import psycopg2
 DBNAME = "news"
 
+
 def get_result(query):
     connection = psycopg2.connect(dbname=DBNAME)
     cursor = connection.cursor()
@@ -9,12 +10,14 @@ def get_result(query):
     connection.close()
     return result
 
-def show_result(result,query_num):
-    for i in range (len(result)):
+
+def show_result(result, query_num):
+    for i in range(len(result)):
         if(query_num == 1 or query_num == 2):
-            print('{0} - {1} views\n'.format(result[i][0],result[i][1]))
+            print('{0} - {1} views\n'.format(result[i][0], result[i][1]))
         else:
-            print('{0} - {1}% errors'.format(result[i][0],result[i][1]))
+            print('{0} - {1}% errors'.format(result[i][0], result[i][1]))
+
 
 request_1 = "What are the most popular three articles of all time?\n"
 
@@ -31,18 +34,19 @@ query_2 = "Select authors.name,count(log.path) as views from authors \
 
 request_3 = "On which days did more than 1% of requests lead to errors?\n"
 
-query_3 = "select to_char(A.time,'Mon DD,YYYY'), round((A.requestfailed*100.00/B.requests),3) \
+query_3 = "select to_char(A.time,'Mon DD,YYYY'), \
+           round((A.requestfailed*100.00/B.requests),3) \
            as errors from failedlog A join requestlog B on A.time=B.time \
            where (A.requestfailed*100.00/B.requests)>1.00;"
 
 print(request_1)
 result = get_result(query_1)
-show_result(result,1)
+show_result(result, 1)
 
 print(request_2)
 result = get_result(query_2)
-show_result(result,2)
+show_result(result, 2)
 
 print(request_3)
 result = get_result(query_3)
-show_result(result,3)
+show_result(result, 3)
